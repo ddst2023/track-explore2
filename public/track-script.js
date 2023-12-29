f088a649df58bc4bb91cb273859151817e1a73c8
ddltv <dehan@shoptoken.com>
2023-12-28 22:47:02 -0500

    Script fix 3

--- public/track-script.js
+++ public/track-script.js
@@ -1,15 +1,16 @@
 var LTV = {
   saveConfirmationData: function (retailerID, onSuccess, onFailure) {
+    const data = { retailerID, pageData: document.body.innerHTML };
     fetch("https://track-explore2.vercel.app/api/tracking-recorder", {
       method: "post",
-      body: JSON.stringify({ retailerID, pageData: document.body.innerHTML }),
+      body: JSON.stringify(data),
     })
       .then(function () {
-        console.log("page persist success");
+        console.log("page persist success", { data });
         onSuccess();
       })
       .catch(function () {
-        console.log("page persist failed");
+        console.log("page persist failed", { data });
         onFailure();
       });
   },
