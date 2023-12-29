20d9c2688e405c0ab05c3b405c7263af65f683ff
ddltv <dehan@shoptoken.com>
2023-12-28 22:04:09 -0500

    Move script inside function

--- public/track-script.js
+++ public/track-script.js
@@ -1,10 +1,10 @@
-fetch("https://track-explore2.vercel.app/api/tracking-recorder", {
-  method: "post",
-  body: JSON.stringify({ pageData: document.body.innerHTML }),
-})
-  .then(function resp1(res) {
-    return res.json();
-  })
-  .then(function resp2(jsn) {
-    console.log(jsn);
-  });
+var LTV = {
+  saveConfirmationData: function (retailerID, onSuccess, onFailure) {
+    fetch("https://track-explore2.vercel.app/api/tracking-recorder", {
+      method: "post",
+      body: JSON.stringify({ pageData: document.body.innerHTML, retailerID }),
+    })
+      .then(onSuccess)
+      .catch(onFailure);
+  },
+};
